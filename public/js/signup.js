a94bc4270c864d45d4e4b453f75d57440a4b0c92
Adam Hussain <adam@hussmedia.io>
2024-05-16 18:38:51 -0700

    Created signup.handlebars, added fields, functionality of routing to signup page from login page

---
+++ public/js/signup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Event listener for the login button
+    document.querySelector('#login').addEventListener('click', function() {
+      window.location.href = '/login'; // Redirect to login page
+    });
+  });
