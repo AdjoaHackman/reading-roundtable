9c35f5e739f2f3c062b0c1e8ad3adf19bd056d60
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-05-17 17:00:18 -0700

    added buttons and deleted stuff

--- models/index.js
+++ models/index.js
@@ -1,19 +1,6 @@
 const User = require('./User');
 const Book = require("./Book");
-const Opinion = require("./opinion");
 
-User.hasMany(Opinion, {
-    foreignKey: "user_id"
-});
-Opinion.hasOne(User, {
-    foreignKey: "user_id"
-});
 
-Book.hasMany(Opinion, {
-    foreignKey: "book_id"
-});
-Opinion.hasOne(Book, {
-    foreignKey: "book_id"
-});
 
-module.exports = { User, Book, Opinion };
+module.exports = { User, Book };
