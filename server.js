cd17c2ffe8eb457773b706a58870c8f02a581300
Alfred Wallace <contact@alfredwallace.me>
2024-05-19 18:03:00 -0700

    actual port fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 const hbs = exphbs.create({ helpers });
 
