84fd1b307c73e99fb275df509497e104246eca9b
Alfred Wallace <contact@alfredwallace.me>
2024-05-19 19:21:57 -0700

    maybe jawsdb was a mistake

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
+if (false) //process.env.JAWSDB_URL) {
+{
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
