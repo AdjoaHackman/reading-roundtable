ae72851941ad972157fc4402087bfe58234803dc
Alfred Wallace <contact@alfredwallace.me>
2024-05-19 19:37:41 -0700

    Capitalizing PORT but also forcing a push

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// capitalizing process.env.PORT may be mandatory for Heroku
 
 const hbs = exphbs.create({ helpers });
 
